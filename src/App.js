0d6dc794da08f7d3958c88e2ba0d6f402ff5cf40
ummarFurquan <mohammedummar29@gmail.com>
2024-05-23 16:18:00 +0530

    Remove Existing files and Add Tailwind

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './Components/Main/Main';
-import About from './Components/About/About';
+
 
 function App() {
   return (
     <>
     <div>
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Main/>}/>
-      <Route path='/about' element={<About/>}/>
-    </Routes>
-    </BrowserRouter>
+      <Navbar/>
+   
     </div>
     
     </>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,36 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../../assets/logo.jpg.jpg';
-import styles from './Navbar.module.css';
+import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className={styles.header}>
-      <img className={styles.logo} src={Logo} alt='Logo' />
-      <nav>
-        <ul className={styles.nav_links}>
-          <li className={styles.links}>
-          <Link to="/" aria-label="About Us">Home</Link>
-          </li>
-          <li className={styles.links}>
-            <Link to="/about" aria-label="About Us">About Us</Link>
-          </li>
-          <li className={styles.links}>
-            <Link to="/services" aria-label="Services">Services</Link>
-          </li>
-          <li className={styles.links}>
-            <Link to="/approach" aria-label="Our Approach">Our Approach</Link>
-          </li>
-          <li className={styles.links}>
-            <Link to="/capabilities" aria-label="Capabilities">Capabilities</Link>
-          </li>
-        </ul>
-      </nav>
-      <a className={styles.cta} href="/contact" aria-label="Contact Us">
-        <button className={styles.contact}>Contact Us</button>
-      </a>
-    </header>
-  );
-};
+    <div>
+      Navbar
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
