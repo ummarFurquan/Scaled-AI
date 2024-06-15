053b8de6f9b0af1e35f293eb14067fad093e7815
ummarFurquan <mohammedummar29@gmail.com>
2024-06-15 14:37:26 +0530

    Added Error Boundary

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import About from "./Components/About/About";
 import Approach from "./Components/Approach/Approach";
 import Footer from "./Components/Footer/Footer";
 import Contact from "./Components/Contact/Contact";
+import { ErrorBoundary } from "react-error-boundary";
+import Error from './Components/Error';
+
 function App() {
   return (
     <>
@@ -13,13 +16,20 @@ function App() {
         <Navbar />
         <Routes>
           {/* Hero Element is in the Main folder, You can find multiple components in the Hero component */}
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/approach" element={<Approach />} />
 
-          <Route path="/" element={<Hero />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/approach" element={<Approach />}></Route>
-
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Navigate to="/" replace />}></Route>
+          <Route 
+            path="/contact" 
+            element={
+              <ErrorBoundary fallback={<Error />}>
+                <Contact />
+              </ErrorBoundary>
+            } 
+          />
+          
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
         <Footer />
       </BrowserRouter>
