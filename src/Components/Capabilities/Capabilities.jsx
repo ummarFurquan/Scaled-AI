4bfb14f337e75e384d90afcb3c558b3536b72c6d
ummarFurquan <mohammedummar29@gmail.com>
2024-06-16 12:02:33 +0530

    Add Capabilities Component

---
+++ src/Components/Capabilities/Capabilities.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Capabilities = () => {
+  return (
+    <div className='container mx-5 my-5'>
+      <h2 style={{overflow:'hidden'}}>Capabilities</h2>
+    </div>
+  )
+}
+
+export default Capabilities
