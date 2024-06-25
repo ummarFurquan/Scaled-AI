e776077045fc0b9b02cb903d3c467c25fe0a61d5
ummarFurquan <mohammedummar29@gmail.com>
2024-06-25 14:04:08 +0530

    Style Changes

--- src/Components/Main/Hero.jsx
+++ src/Components/Main/Hero.jsx
@@ -13,12 +13,12 @@ const Hero = () => {
                     <div className='text-container'>
                         <div className='text-hero-bold'>
                             <span className='top-heading'>
-                                Empowerment through <span>Technology</span> <br />
-                                Impact through <span>Solutions</span>
+                                Empowerment through <span style={{color:'red'}}>Technology</span> <br />
+                                Impact through <span style={{color:'red'}}>Solutions</span>
                             </span>
                         </div>
                         <div className='text-hero-regular'>
-                            In today's fast-paced digital landscape, agile adaptation to emerging technologies is crucial for staying competitive. At Agilitnet, we specialize in providing digital engineering and IT solutions that enable businesses to maneuver through these changes successfully. With a focus on outcome-driven approaches, we help our clients capitalize new opportunities and attain sustainable growth.
+                            In today's fast-paced digital landscape, agile adaptation to emerging technologies is crucial for staying competitive. At ScaledAI, we specialize in providing digital engineering and IT solutions that enable businesses to maneuver through these changes successfully. With a focus on outcome-driven approaches, we help our clients capitalize new opportunities and attain sustainable growth.
                         </div>
                         <div className='cta'>
                             <a href='#main' className='btn btn-danger'>Explore More</a>
