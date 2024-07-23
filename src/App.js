b17ad11c0b5292484b3622b4ed6084e58f01eaf3
ummarFurquan <mohammedummar29@gmail.com>
2024-07-23 12:42:19 +0530

    Changes!

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Main/Hero';
-import About from './Components/About/About';
-import Footer from './Components/Footer/Footer';
-import Contact from './Components/Contact/Contact';
-import { ErrorBoundary } from 'react-error-boundary';
-import Error from './Components/Error';
-import Capabilities from './Components/Capabilities/Capabilities';
-import ScrollToTop from 'react-scroll-to-top';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+// import './App.css';
+// import Navbar from './Components/Navbar/Navbar';
+// import Hero from './Components/Main/Hero';
+// import About from './Components/About/About';
+// import Footer from './Components/Footer/Footer';
+// import Contact from './Components/Contact/Contact';
+// import { ErrorBoundary } from 'react-error-boundary';
+// import Error from './Components/Error';
+// import Capabilities from './Components/Capabilities/Capabilities';
+// import ScrollToTop from 'react-scroll-to-top';
 
-function App() {
-  return (
-    <>
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route
-            path="/contact"
-            element={
-              <ErrorBoundary FallbackComponent={Error}>
-                <Contact />
-              </ErrorBoundary>
-            }
-          />
-          <Route path="/capabilities" element={<Capabilities />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-        <Footer />
-      </div>
-      <ScrollToTop
-      color='#dc3545'
-      />
-    </Router>
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//     <Router>
+//       <div className="App">
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Hero />} />
+//           <Route path="/about" element={<About />} />
+//           <Route
+//             path="/contact"
+//             element={
+//               <ErrorBoundary FallbackComponent={Error}>
+//                 <Contact />
+//               </ErrorBoundary>
+//             }
+//           />
+//           <Route path="/capabilities" element={<Capabilities />} />
+//           <Route path="*" element={<Navigate to="/" replace />} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//       <ScrollToTop
+//       color='#dc3545'
+//       />
+//     </Router>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
